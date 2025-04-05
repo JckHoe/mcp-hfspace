c08252ed4544af2a0043221c118926008ab0845a
Ong Jack Hoe <ongjackhoe@gmail.com>
2025-04-05 18:04:44 +0900

    Feature/export mode handling (#1)
    * Export mode to allow no export to file on result from HF
    
    * update README
    
    * update packagejson for this forked version
    
    * update config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,22 @@
 import minimist from "minimist";
 import path from "path";
 
+// Export types enum
+export enum ExportMode {
+  FileBased = "FileBased", // Default
+  None = "None",
+}
+
+function isExportMode(value: unknown): value is ExportMode {
+  return (
+    typeof value === "string" &&
+    Object.values(ExportMode).includes(value as ExportMode)
+  );
+}
+
 export interface Config {
   claudeDesktopMode: boolean;
+  exportMode: ExportMode;
   workDir: string;
   spacePaths: string[];
   hfToken?: string;
@@ -13,10 +27,11 @@ export const config = parseConfig();
 
 export function parseConfig(): Config {
   const argv = minimist(process.argv.slice(2), {
-    string: ["work-dir", "hf-token"],
+    string: ["work-dir", "hf-token", "export-mode"],
     boolean: ["desktop-mode", "debug"],
     default: {
       "desktop-mode": process.env.CLAUDE_DESKTOP_MODE !== "false",
+      "export-mode": process.env.MCP_HF_EXPORT_MODEL || ExportMode.FileBased,
       "work-dir": process.env.MCP_HF_WORK_DIR || process.cwd(),
       "hf-token": process.env.HF_TOKEN,
       debug: false,
@@ -24,8 +39,13 @@ export function parseConfig(): Config {
     "--": true,
   });
 
+  const rawExportMode = argv["export-mode"];
+  const exportMode = isExportMode(rawExportMode)
+    ? rawExportMode
+    : ExportMode.FileBased;
   return {
     claudeDesktopMode: argv["desktop-mode"],
+    exportMode: exportMode,
     workDir: path.resolve(argv["work-dir"]),
     hfToken: argv["hf-token"],
     debug: argv["debug"],
